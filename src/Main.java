25f1fab0bddca0ce3ed79003b349b150657cf299
yjh <2138870753@qq.com>
2023-09-12 11:35:28 +0800

    运行

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 // 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
 // 然后按 Enter 键。现在，您可以在代码中看到空格字符。
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Test test=new Test();
@@ -8,3 +9,4 @@ public class Main {
         test.test1();
     }
 }
+
